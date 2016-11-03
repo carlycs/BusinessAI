5208518709bdd2965d23dcb60837a45ff51d18ca
Lotus Group Business AI Computing Page <hp@lotustech.io>
2016-11-03 15:00:14 -0400

    data summary print out

--- airbnb/airbnb.py
+++ airbnb/airbnb.py
@@ -14,3 +14,7 @@ import seaborn as sns
 train_users = pd.read_csv('./data/train_users.csv')
 test_users = pd.read_csv('./data/test_users.csv')
 
+#data summary
+print("We have", train_users.shape[0], "users in the training set and", 
+      test_users.shape[0], "in the test set.")
+print("In total we have", train_users.shape[0] + test_users.shape[0], "users.")
