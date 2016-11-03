a9cba6e8f250f6189104fd777497af243235280f
Lotus Group Business AI Computing Page <hp@lotustech.io>
2016-11-03 15:03:48 -0400

    treatment the data and missing data

--- airbnb/airbnb.py
+++ airbnb/airbnb.py
@@ -18,3 +18,20 @@ test_users = pd.read_csv('./data/test_users.csv')
 print("We have", train_users.shape[0], "users in the training set and", 
       test_users.shape[0], "in the test set.")
 print("In total we have", train_users.shape[0] + test_users.shape[0], "users.")
+
+#missing data treatment
+users.gender.replace('-unknown-', np.nan, inplace=True)
+
+users_nan = (users.isnull().sum() / users.shape[0]) * 100
+users_nan[users_nan > 0].drop('country_destination')
+
+print("Just for the sake of curiosity; we have", 
+      int((train_users.date_first_booking.isnull().sum() / train_users.shape[0]) * 100), 
+      "% of missing values at date_first_booking in the training data")
+
+
+#The other feature with a high rate of NaN was age. Let's see:
+users.age.describe()
+
+print(sum(users.age > 122))
+print(sum(users.age < 18))
