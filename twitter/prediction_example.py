686a59cdf8706fdf21dc7f96fc988282f5f46e05
Lotus Group Business AI Computing Page <hp@lotustech.io>
2016-11-03 10:31:17 -0400

    code header setup

---
+++ twitter/prediction_example.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+# -*- coding: utf8 -*-
+
+# SAMPLE SCRIPT TO TEST THE TWITTER DATA FOR 'Influencers in a Social Network' research.
+# 
+# written by Hoofar Pourzand 
+# Used part of the code from Ferenc Huszár at PeerIndex
